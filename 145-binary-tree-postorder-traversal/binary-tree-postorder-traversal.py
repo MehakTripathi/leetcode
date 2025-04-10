25b6abf23d8f130098aa2cb16a6e6af7cdfedcfd
MehakTripathi <mehaktripathi0707@gmail.com>
2025-04-10 18:17:35 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (54.76%) - LeetSync

--- 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.py
+++ 145-binary-tree-postorder-traversal/binary-tree-postorder-traversal.py
@@ -6,7 +6,25 @@
 #         self.right = right
 class Solution:
     def postorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        if not root:
+            return []
+
         ans=[]
+        st1, st2=[root], []
+
+        while st1:
+            node=st1.pop()
+            st2.append(node.val)
+
+            if node.left:
+                st1.append(node.left)
+            if node.right:
+                st1.append(node.right)
+                
+        return st2[::-1]
+
+
+        '''ans=[]
         def postorder(root):
             if not root:
                 return
@@ -15,5 +33,5 @@ class Solution:
             ans.append(root.val)
 
         postorder(root)
-        return ans
+        return ans'''
         
