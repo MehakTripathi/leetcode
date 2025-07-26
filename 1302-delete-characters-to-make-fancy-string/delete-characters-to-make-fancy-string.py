1575adb0bd437c38920434daa7199880efbc1338
MehakTripathi <mehaktripathi0707@gmail.com>
2025-07-26 23:05:46 +0530

    Time: 484 ms (62.32%) | Memory: 17.5 MB (16.32%) - LeetSync

--- 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.py
+++ 1302-delete-characters-to-make-fancy-string/delete-characters-to-make-fancy-string.py
@@ -1,11 +1,15 @@
 class Solution(object):
     def makeFancyString(self, s):
         res = []
+        count=1
 
-        for ch in s:
-            if len(res) >= 2 and res[-1] == ch and res[-2] == ch:
-                continue
+        for i in range(1, len(s)):
+            if s[i] ==s[i-1]:
+                count +=1
+            else:
+                count=1
 
-            res.append(ch)
-
+            if count<3:
+                res.append(s[i-1])
+        res.append(s[-1])
         return ''.join(res)
