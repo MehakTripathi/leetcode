271cce6df774de40a9ded43aa9e9a7c53b5a8079
MehakTripathi <mehaktripathi0707@gmail.com>
2025-08-16 14:59:26 +0530

    Time: 0 ms (100.00%) | Memory: 17.9 MB (6.80%) - LeetSync

---
+++ 1448-maximum-69-number/maximum-69-number.py
@@ -0,0 +1,13 @@
+class Solution:
+    def maximum69Number (self, num: int) -> int:
+        a= list(str(num))
+
+
+        for i in range(len(a)):
+            if a[i]=='6':
+                a[i]= '9'
+                break
+
+        return int("".join(a))
+
+        
